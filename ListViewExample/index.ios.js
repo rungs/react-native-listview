6541fbc9d492049216d563bbde1bcec24703a7fe
Rungaroon Saiphat <rungaroon.saiphat@gmail.com>
2016-06-04 19:26:49 +0700

    import mock data to index.ios.js and import image component

--- ListViewExample/index.ios.js
+++ ListViewExample/index.ios.js
@@ -9,23 +9,16 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
+import MockData from './mockdata';
 
 class ListViewExample extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+
       </View>
     );
   }
